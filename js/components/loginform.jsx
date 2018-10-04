4658806583bd4b237cbab6b040c0d18255691efe
pawelweglowski <pawel.weglowski@hotmail.com>
2018-10-04 11:17:38 +0200

    few  more sketches but visualisation done

--- js/components/loginform.jsx
+++ js/components/loginform.jsx
@@ -17,51 +17,15 @@ class Loginform extends React.Component{
     
     
     render(){
-        const calculeteStrength = function (password) {
-            let strength = 0;
-             const strengthRules = [
-                /[A-Z]/,
-                /[a-z]/,
-                /[0-9]/,
-                /.{8}/, 
-                /.{14}/,
-                /[!-//:-@[-`{-ÿ]/
-            ];
-            strengthRules.forEach(function (strengthRules) {
-                if (strengthRules.test(password)) {
-                    strength++;
-                }
-            });
-            return {
-                value: strength,
-                max: strengthRules.length
-            };
-        };
-        const checkPasswordStrength = function (password) {
-            let progress = document.querySelector('passwordStrength'), strength = calculeteStrength(this.value);
-            progress.value = strength.value;
-            progress.max = strength.max;
-        };
-        const showProgress = function() {
-            let progress = document.querySelector('progress');
-            progress.style.visibility = 'visible';
-        };
-        const hideProgress = function() {
-            let progress = document.querySelector('progress');
-            progress.style.visibility = 'hidden';
-        };
-        let input = document.querySelector('passwordInput');
-        input.addEventListener('keyup', checkPasswordStrength);
-        input.addEventListener('click', showProgress);
-        input.addEventListener('glur', Progress);
+       
         
         return (
-            <div className="container">
+            <div id="container">
 		        <form>		
-			        <input type="text" placeholder="login">		
-			        <input type="password" placeholder="password" onfocus="this.placeholder" onblur="this.placeholder" id="passwordInput">			
+                    <input type="text" placeholder="login"></input>	
+                    <input type="password" placeholder="password" onFocus="this.placeholder" onBlur="this.placeholder" id="passwordInput"></input>		
 			        <progress id="passwordStrength" value="0"></progress>
-			        <input type="submit" value="Sign In">			
+                    <input type="submit" value="Sign In"></input>			
 		        </form>		
 	        </div>
         )
